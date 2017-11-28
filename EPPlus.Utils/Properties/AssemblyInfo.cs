f975ad2049a37193d61fdd9386efa5409ac3d4b1
FireAwayH <hejiheji001@gmail.com>
2017-11-28 15:36:01 +0800

    Init

--- EPPlus.Utils/Properties/AssemblyInfo.cs
+++ EPPlus.Utils/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EPPlus.CSVParser")]
+[assembly: AssemblyTitle("EPPlus.Utils")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EPPlus.CSVParser")]
+[assembly: AssemblyProduct("EPPlus.Utils")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
